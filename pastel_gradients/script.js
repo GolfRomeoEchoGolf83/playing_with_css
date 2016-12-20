f2b9803d857bd7c265702fa85a635862a193314e
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2016-12-20 14:31:45 +0100

    Swapping between files (code into script file was inteded to be in stylesheet)

--- pastel_gradients/script.js
+++ pastel_gradients/script.js
@@ -1,31 +1,3 @@
 /**
  * Created by Greg on 20/12/2016.
  */
-body {}
-
-h1, p {}
-
-header {}
-
-.container {}
-
-.box {}
-
-.box p {}
-
-/* Gradient 1 */
-#g1 {}
-/* Gradient 2 */
-#g2 {}
-/* Gradient 3 */
-#g3 {}
-/* Gradient 4 */
-#g4 {}
-/* Gradient 5 */
-#g5 {}
-/* Gradient 6 */
-#g6 {}
-/* Gradient 7 */
-#g7 {}
-/* Gradient 8 */
-#g8 {}
